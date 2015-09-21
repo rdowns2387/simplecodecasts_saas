26be75339f323a09faf06f13f4e8535e14368cc1
Ryan Downs <redowns2@gmail.com>
2015-09-21 22:33:22 +0000

    Working through heroku push error

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,6 @@ class ContactsController < ApplicationController
             name = params[:contact][:name]
             email = params[:contact][:email]
             body = params[:contact][:comments]
-            
             ContactMailer.contact_email(name, email, body).deliver
             flash[:success] = "Message sent."
             redirect_to new_contact_path
